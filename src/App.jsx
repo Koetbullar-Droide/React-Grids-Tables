3dbd4d7a3dda2483414d9ac15279e845bd0bf5b6
Jannik Luethi <jannik.luethi@swisscom.com>
2024-05-23 13:53:22 +0200

    fetch & json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
+
 
 
 function App() {
-  
 
+  const [columns, setColumns] = useState([]);
+  
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      try {
+        const response = await fetch('http://localhost:5000/players');
+        const data = await response.json();
+        
+        
+        setColumns(Object.keys(data[0]).map((key) => {
+          return {
+            field: key,
+            headerName: key,
+            width: 130,
+          }
+        }));
+        setRows(data.map((row, index) => {
+          return { id: index, ...row }
+        }));
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+
+  }, []);
 
   return (
     <>
